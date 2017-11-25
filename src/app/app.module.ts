bd4069853bbdc69c597aabdfec8e6a241247bc1e
Sami-Dghim <samidghim@gmail.com>
2017-11-25 22:09:37 +0100

    Testing App: Add PhantomJS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
+import { NgModule  } from '@angular/core';
+import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 import { DataService } from './data.service';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,14 +1,22 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
+import { DataService } from '../data.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { trigger,style,transition,animate,keyframes,query,stagger } from '@angular/animations';
+
 
 import { HomeComponent } from './home.component';
 
-describe('HomeComponent', () => {
+xdescribe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      imports: [ FormsModule, BrowserAnimationsModule ],
+      declarations: [ HomeComponent ],
+      providers: [DataService]
+
     })
     .compileComponents();
   }));
